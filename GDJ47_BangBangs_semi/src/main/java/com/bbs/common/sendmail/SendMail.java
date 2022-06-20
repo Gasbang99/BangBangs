1eb89f7705d640c4385c4227dbc4f86f2fc4a1ac
ChungJunYong <ChungJunYong@DESKTOP-HTL8HTM>
2022-06-20 14:46:38 +0900

    JY 0620_1446 COMMIT

--- GDJ47_BangBangs_semi/src/main/java/com/bbs/common/sendmail/SendMail.java
+++ GDJ47_BangBangs_semi/src/main/java/com/bbs/common/sendmail/SendMail.java
@@ -63,7 +63,7 @@ public class SendMail {
 	
 	public static String naverMailSend(String naverAddress) {
         String host = "smtp.naver.com";
-        String user = "lojyve8"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
+        String user = "lojyve8@naver.com"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
         String password = "thswldid4fkdgo*";// 패스워드
 
         // SMTP 서버 정보를 설정한다.
