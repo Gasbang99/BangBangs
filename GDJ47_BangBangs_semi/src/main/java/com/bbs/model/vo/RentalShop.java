fbb021343186a73a2c0f9cfa07d15ec46e107187
Swift3 <Swift3@DESKTOP-R51ITGJ>
2022-06-21 13:34:51 +0900

    hj_22_06_21 13:34 rantalShop 수정

---
+++ GDJ47_BangBangs_semi/src/main/java/com/bbs/model/vo/RentalShop.java
@@ -0,0 +1,21 @@
+package com.bbs.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class RentalShop {
+	private int rentalShopId;
+	private String rentalShopName;
+	private double latitude;
+	private double longitude;
+	private String rentalShopAddress;
+
+}
