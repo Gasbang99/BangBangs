0c66d2e784f2c02dea28037939169351faafe667
Swift3 <Swift3@DESKTOP-R51ITGJ>
2022-06-21 09:49:16 +0900

    hj_22_06_21 09:48 member 수정

--- GDJ47_BangBangs_semi/src/main/java/com/bbs/model/vo/Member.java
+++ GDJ47_BangBangs_semi/src/main/java/com/bbs/model/vo/Member.java
@@ -2,9 +2,13 @@ package com.bbs.model.vo;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class Member {
